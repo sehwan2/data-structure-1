1b75ca1e627d74ef0cabe124ef9eb333363d6008
sehwan2 <75080764+sehwan2@users.noreply.github.com>
2022-06-02 11:29:54 +0900

    Add files via upload

---
+++ circularList.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include<stdlib.h>
+typedef int element;
+typedef struct {
+	element data;
+	struct ListNode* link;
+} ListNode;
+
+void print_list(ListNode* head)
+{
+	ListNode* p;
+	if (head == NULL) return;
+	p = head->link;
+	do {
+		printf("%d->", p->data);
+		p = p->link;
+	} while (p != head);
+	printf("%d->", p->data);
+
+}
+
+
+
+void error(char* message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+ListNode* insert_first(ListNode* head, element data)
+{
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->data = data;
+	if (head == NULL) {
+		head = node;
+		node->link = head;
+	}
+	else {
+		node->link = head->link;
+		head->link = node;
+	}
+	return head;
+}
+
+//노드 pre 뒤에 새로운 노드 삽입
+ListNode* insert_last(ListNode* head, element data)
+{
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->data = data;
+	if (head == NULL) {
+		head = node;
+		node->link = head;
+	}
+	else {
+		node->link = head->link;
+		head->link = node;
+		head = node;
+	}
+	return head;
+}
+
+
+int main(void)
+{
+	ListNode* head = NULL;
+
+	head = insert_last(head, 20);
+	head = insert_last(head, 30);
+	head = insert_last(head, 40);
+	head = insert_first(head, 10);
+	print_list(head);
+	
+
+	return 0;
+
+}
